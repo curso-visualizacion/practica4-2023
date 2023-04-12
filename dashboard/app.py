270b63797b7df87b24d62de690fab1b53e5a22ef
Diego <diegorodriguezmancini@gmail.com>
2023-04-12 19:02:58 -0400

    ejercicio1

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,24 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
+import pandas as pd
+import plotly.express as px
 
 app = Dash(__name__)
-server = app.server
 
-app.layout = html.Div(children="Hello World")
+gapminder = pd.read_csv("dashboard/static/gapminderData2.csv")
+
+scatter = px.scatter(
+    gapminder[gapminder.year == 2007],
+    x="gdpPercap",
+    y="bornPerwom",
+    hover_name="country",
+)
+
+app.layout = html.Div(
+    children=[
+        html.H1("PIB Per cápita vs nacimientos por mujer"),
+        dcc.Graph(id="ejercicio1", figure=scatter),
+    ]
+)
 
 
 if __name__ == "__main__":
