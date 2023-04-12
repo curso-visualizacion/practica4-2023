9c72df27a76ab24edac536fb32324fd45e0f8205
Diego <diegorodriguezmancini@gmail.com>
2023-04-12 19:21:38 -0400

    ejercicio3

--- dashboard/plots.py
+++ dashboard/plots.py
@@ -4,10 +4,14 @@ import pandas as pd
 
 def plot_scatter(data: pd.DataFrame):
     return px.scatter(
-        data[data.year == 2007],
+        data,
         x="gdpPercap",
         y="bornPerwom",
         size="pop",
         color="continent",
         hover_name="country",
+        size_max=50,
+        animation_frame="year",
+        range_y=[0, 9],
+        range_x=[-1000, 60000],
     )
